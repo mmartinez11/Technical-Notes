c2b8b16084710b6097c5c0cbce55d13a4e941e01
Manuel Martinez <33674827+mmartinez11@users.noreply.github.com>
2021-02-09 15:45:35 -0600

    Update DSAlloaction.cpp

--- DSAlloaction.cpp
+++ DSAlloaction.cpp
@@ -14,14 +14,18 @@ struct paperClip{
 int main()
 {
 
-//---------Dynamic/Static Allocation--------------------------------------------
+//---------Dynamic Allocation---------------------------------------------------
     
    //Use the (new) operator to allocate heap memory 
    //Dynamic Array with size of 5
    //arr1 points at a block of memory in the heap
    int *arr1 = new int[5];
    
-   delete arr1;//(delete) used for (new)
+   cout << "(1) Value of arr1 is: " << arr1[0] << endl;
+   cout << "(2) Value of arr1 is: " << *arr1 << endl;
+   
+   //(delete) used for (new) deletes the value being pointed at
+   delete arr1;
 
    //Malloc allocates memory in the heap
    //Functions similar to (new) however for objects malloc cannot call 
@@ -30,12 +34,28 @@ int main()
    int size = 5;
    arr2 = (int *) malloc (sizeof(int) * size);
    
-   free(arr2);//(free) used for (malloc)
+   cout << "(3) Value of arr2 is: " << arr2[0] << endl;
+   cout << "(4) Value of arr2 is: " << *arr2 << endl;
+   
+   //(free) used for (malloc)
+   free(arr2);
+   
+   //This creates a pointer that points to an array of size 5 of type (paperClip)
+   //This is allocated in the heap
+   paperClip *c = new paperClip[5];
+   
+   //The method to access the inner values
+   cout << "(5) The value of x is: " << c[0].x << endl;
+   cout << "(6) The value of y is: " << c->y << endl;
+   
+   //Delete array (new) deletes all values in the array
+   delete[] c;
+
+//---------Static Allocation----------------------------------------------------   
    
    //The ([]) operator is used to allocate a block of memory of a certain size
    //If used statically it will create a block of code on the stack
-   //Variable (a) will be a block of memory with 5 int pointers on the stack
-   int *a[5];
+   //([]) can also dereference without using (*)
    
    //Variable (d) will create a static int array on the stack
    int d[5];
@@ -45,17 +65,32 @@ int main()
    paperClip b[5];
    
    //The method to access the inner values
-   cout << b[0].x << endl;
+   cout << "(7) The value of x is: " << b[0].x << endl;
    
-   //This creates a pointer that points to an array of size 5 of type (paperClip)
-   //This is allocated in the heap
-   paperClip *c = new paperClip[5];
+   cout << "(8) The value of y is: " << b->y << endl;
    
-   //The method to access the inner values
-   cout << c[0].y << endl;
-   
-   //Delete array (new)
-   delete[] c;
-    
+//---------Allocation of Pointers-----------------------------------------------
+
+ //Creates an array of pointers of type int on the stack
+ int* arr3[3];
+ int a = 5;
+ 
+ //Int pointer at position (0) assigned to point to variable a
+ arr3[0] = &a;
+ 
+ //(arr[0]) returns an address which must then be dereferenced
+ cout << "(9) The value of a is: " << *arr3[0] << endl;
+
+ //Creates an array of pointers type int in the heap
+ int **arr4 = new int*[3];
+ int cc = 7;
+ 
+ //Int pointer at position (0) assigned to point to variable a
+ arr4[0] = &cc;
+ 
+ //(arr[0]) returns an address which must then be dereferenced
+ cout << "(10) The value of cc is: " << *arr4[0] << endl;
+ 
+ 
    return 0;
 }
